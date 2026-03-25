085a31633ad937ae6272b50018a770b8184d1bf8
wolf by logan technology <support@wolflogan.com>
2026-03-25 00:58:33 -0400

    Fix admin backend when location settings rows are missing

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const APP_CONFIG = {
-  appsScriptUrl: "https://script.google.com/macros/s/AKfycbxPiFcSqnf4Msn-K7CvcXt0Nil4U5-7832WzaGyUE9eR5RZtiH_LRVrjT8OJu17CM-XeQ/exec",
+  appsScriptUrl: "https://script.google.com/macros/s/AKfycbwDKaNvlOoyd7_PyiyreukPvBTIvNfp7og-TsrGkiOyWFt_5sp_MJ-84TcDXabe1luXtg/exec",
   sheetId: "1C5tYsqvyfBSa386vgQwyvnTluuDrNvC5pHyycV_oNIk",
   brandName: "Botte Scheduling",
   logoPath: "./assets/botte-logo.png",
