9e3b150d4b042a81bb2b486e033d801a635e9a23
wolf by logan technology <support@wolflogan.com>
2026-03-24 23:53:38 -0400

    Add approval inbox and multi-location access controls

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const APP_CONFIG = {
-  appsScriptUrl: "https://script.google.com/macros/s/AKfycby25H66YvR1yJUvbYa5fl1duM9nWf_wd4L22athL5wzm67AIwQsXT4j2ItNktJtAHoaYA/exec",
+  appsScriptUrl: "https://script.google.com/macros/s/AKfycbzoHX9v-p2kkyOW80QrDLHORgs5KvVxdYuOkpmlxTP0odVe0saYEQrB0js7E51cUshzvQ/exec",
   sheetId: "1C5tYsqvyfBSa386vgQwyvnTluuDrNvC5pHyycV_oNIk",
   brandName: "Botte Scheduling",
   logoPath: "./assets/botte-logo.png",
